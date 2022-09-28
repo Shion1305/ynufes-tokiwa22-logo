7abb4d3684a2342ad80b9e668fee6f27e6512b7a
Shion Ichikawa <shion1305@gmail.com>
2022-09-28 20:43:50 +0900

    ✨ ローディング画面をVueシステム外にて生成

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,33 @@
 <template>
   <div id="wrapper">
-    <div id="loader">
-      <LogoAnimation id="logo-animation"/>
-      <div id="logo-text">横浜国立大学<br>2022年常盤祭</div>
-      <div id="logo-date">10/28,29,30</div>
-    </div>
+    なかみなかみ
+    <!--    <div id="loader">-->
+    <!--      <LogoAnimation id="logo-animation"/>-->
+    <!--      <div id="logo-text">横浜国立大学<br>2022年常盤祭</div>-->
+    <!--      <div id="logo-date">10/28,29,30</div>-->
+    <!--    </div>-->
   </div>
 </template>
 
 <script>
-import LogoAnimation from './components/LogoAnimation.vue'
-
-export default {
-  name: 'App',
-  components: {
-    LogoAnimation
-  }
+window.onload = function () {
+  const loader = document.getElementById('loader');
+  loader.classList.add('loaded');
 }
 </script>
 
 <style>
+#wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
 body {
   margin: 0;
   height: 100%;
   background: #051228;
   color: white;
 }
-
-#logo-animation {
-  width: min(65vw, 350px);
-}
-
-#logo-date {
-  font-size: 1.5em;
-}
-
-#logo-text {
-  font-size: min(8vw, 2em);
-}
-
 #app {
   font-family: 'Klee One', cursive;
   -webkit-font-smoothing: antialiased;
